7e0888c5da9fc9428d220a7afba47a1496a1c0c0
juanid <junaidkhana22511@gmail.com>
2022-09-18 23:26:03 +0500

    firstt commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'own':['Ubuntu','sans-serif'], 
+      },
+    },
+  },
+  plugins: [],
+}
